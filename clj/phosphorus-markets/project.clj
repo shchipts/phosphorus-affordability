2b9e67008629ccf71027506a10270b34c6549a01
SHCHIPTSOVA Anna <shchipts@iiasa.ac.at>
2020-05-15 20:30:49 +0200

    Clojure project initialization for P markets modeling

---
+++ clj/phosphorus-markets/project.clj
@@ -0,0 +1,7 @@
+(defproject org.iiasa/phosphorus-markets "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :main nil)

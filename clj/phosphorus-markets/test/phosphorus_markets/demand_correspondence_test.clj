5055a7afda5d000e116468cc86bb981769ca2ef2
Anna Shchiptsova <shchipts@iiasa.ac.at>
2020-05-23 23:55:10 +0200

    Copyright added to tests

--- clj/phosphorus-markets/test/phosphorus_markets/demand_correspondence_test.clj
+++ clj/phosphorus-markets/test/phosphorus_markets/demand_correspondence_test.clj
@@ -1,3 +1,11 @@
+;   Copyright (c) 2020 International Institute for Applied Systems Analysis.
+;   All rights reserved. The use and distribution terms for this software
+;   are covered by the MIT License (http://opensource.org/licenses/MIT)
+;   which can be found in the file LICENSE at the root of this distribution.
+;   By using this software in any fashion, you are agreeing to be bound by
+;   the terms of this license.
+;   You must not remove this notice, or any other, from this software.
+
 (ns phosphorus-markets.demand-correspondence-test
   (:require [clojure.test :refer :all]
             [phosphorus-markets.demand-correspondence :refer :all]))

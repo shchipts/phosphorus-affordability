a36f036df83505b58a31cb667963555d06d7e292
Anna Shchiptsova <shchipts@iiasa.ac.at>
2020-09-13 21:33:49 +0200

    R data package with trade volumes

---
+++ R/dapmap.markets/R/data.R
@@ -0,0 +1,91 @@
+NULL
+
+#' DAP/MAP fertilizer trade volumes 2013-2017
+#'
+#' This compilation provides records on the regional DAP/MAP fertilizer trade volumes estimated from 
+#' the UN Comtrade data, IFA Supply Database and data announced by global phosphate companies. 
+#' 
+#' The following definitions apply: DAP - diammonium phosphate, MAP - monoammonium phosphate.
+#'
+#' This compilation covers the period from 2013 to 2017.
+#'
+#' Countries are reported by ISO 3166 country code.
+#'
+#' This compilation covers the following phosphate companies and groups of companies:
+#' \itemize{
+#'   \item China (export): Chinese exporters
+#'   \item Maaden: Maaden website: https://www.maaden.com.sa/
+#'   \item Mosaic: Mosaic website: http://www.mosaicco.com/
+#'   \item PhosAgro/EuroChem: PhosAgro, EuroChem website: https://phosagro.com/, https://www.eurochemgroup.com/
+#'   \item OCP: OCP website: https://www.ocpgroup.ma/
+#' }
+#'
+#' The fertilizer sales volume from regional producers and other importers are recorded under Home and Other Imports; the values are estimated as the difference between consumption reported in the IFA Supply Database and total sales of the global companies into a region.
+#'
+#' Metric tonnes of P2O5 per annum are used for volumes throughout this compilation.
+#' Original data values are recorded in kilograms in the UN Comtrade Database.
+#' Original data values are recorded in metric tonnes of product in the IFA Supply Database and in the compiled global phosphate companies data.
+#'
+#' The following conversion factors of product to P2O5 volumes were applied:
+#' \itemize{
+#'   \item DAP   0.46
+#'   \item MAP   0.52
+#'   \item MAP (China)   0.44
+#'   \item DAP/MAP   0.49
+#' }
+#'
+#' @docType data
+#'
+#' @format A data frame with 30 rows and 11 variables
+#' \itemize{
+#'   \item Producer: company or a group of companies
+#'   \item Year: year of operations
+#'   \item Africa: producer's trade volume with countries
+#' AGO, BDI, BEN, BFA, CAF, CIV, CMR, COD, COG, DJI, DZA, EGY, ERI, ETH, GAB, GHA, GIN, GMB, GNQ, KEN, LBR, LBY, MAR, MDG, MLI, MOZ, MRT, MUS, MWI, NAM, NER, NGA, RWA, SDN, SEN, SLE, SOM, SWZ, TCD, TGO, TUN, TZA, UGA, ZAF, ZMB, ZWE
+#'   \item East Asia: producer's trade volume with countries
+#' CHN, BRN, IDN, JPN, KHM, KOR, LAO, MMR, MNG, MYS, PHL, PRK, SGP, THA, TLS, VNM
+#'   \item Eastern Europe and Central Asia: producer's trade volume with countries
+#' ARM, AZE, BLR, EST, GEO, KAZ, KGZ, LTU, LVA, MDA, RUS, TJK, TKM, UKR, UZB
+#'   \item Latin America: producer's trade volume with countries
+#' ARG, ATG, BHS, BLZ, BMU, BOL, BRA, BRB, CHL, COL, CRI, CUB, CYM, DMA, DOM, ECU, GRD, GTM, GUY, HND, HTI, JAM, KNA, LCA, MEX, NIC, PAN, PER, PRY, SLV, SUR, TCA, TTO, URY, VCT, VEN
+#'   \item North America: producer's trade volume with countries
+#' CAN, USA
+#'   \item Oceania: producer's trade volume with countries
+#' AUS, FJI, NCL, NRU, NZL, PLW, PNG, PYF, SLB
+#'   \item South Asia: producer's trade volume with countries
+#' BGD, IND, LKA, NPL, PAK
+#'   \item Western and Central Europe: producer's trade volume with countries
+#' ALB, AUT, BEL, BGR, BIH, CHE, CZE, DEU, DNK, ESP, FIN, FRA, GBR, GRC, HRV, HUN, IRL, ISL, ITA, LUX, MKD, MNE, NLD, NOR, POL, PRT, ROU, SRB, SVK, SVN, SWE
+#'   \item Western Asia: producer's trade volume with countries
+#' AFG, ARE, BHR, CYP, IRN, IRQ, ISR, JOR, KWT, LBN, OMN, PSE, QAT, SAU, SYR, TUR, YEM
+#' }
+#'
+#' @references 
+#' \itemize{
+#'   \item UN Comtrade Database, accessed from https://comtrade.un.org/
+#'
+#' 	 \item IFA Supply Database, accessed from https://www.ifastat.org/supply/Phosphate%20Products/Processed%20Phosphates\item EuroChem 2013 Annual Report And Accounts: "Helping The World Grow". Downloaded from https://www.eurochemgroup.com/investors/reports-results/
+#' 	 \item EuroChem Q4 2014 Press Release: "EuroChem Group AG Reports IFRS Financial Information for 2014". Downloaded from https://www.eurochemgroup.com/investors/reports-results/
+#' 	 \item EuroChem 2016 Annual Report And Accounts: "Keeping Our Focus". Downloaded from https://www.eurochemgroup.com/investors/reports-results/
+#' 	 \item EuroChem 2017 Annual Report And Accounts: "Building Momentum". Downloaded from https://www.eurochemgroup.com/investors/reports-results/
+#'
+#' 	 \item Maaden Annual Report 2013: "Driving Development". Downloaded from https://www.maaden.com.sa/en/investor/report
+#' 	 \item Maaden Annual Report 2014: "Delivering Results". Downloaded from https://www.maaden.com.sa/en/investor/report
+#' 	 \item Maaden Annual Report 2015: "Mining For Shareholder Value And National Development". Downloaded from https://www.maaden.com.sa/en/investor/report
+#' 	 \item Maaden Annual Report 2016: "Facing Today's Challenges". Downloaded from https://www.maaden.com.sa/en/investor/report
+#' 	 \item Maaden Annual Report 2017: "Building A Sustainable Future". Downloaded from https://www.maaden.com.sa/en/investor/report
+#'
+#' 	 \item CF Industries 2013 Annual Report: "Momentum". Downloaded from http://www.snl.com/IRW/FinancialDocs/4533245
+#' 	 \item Mosaic 2015 Annual Report. Downloaded from http://investors.mosaicco.com/CorporateProfile
+#' 	 \item Mosaic 2016 Annual Report. Downloaded from http://investors.mosaicco.com/CorporateProfile
+#' 	 \item Mosaic 2017 Annual Report. Downloaded from http://investors.mosaicco.com/CorporateProfile
+#'
+#' 	 \item OCP S.A. Note d'information - Emission Obligataire OCP SA 2018. Downloaded from http://www.casablanca-bourse.com/bourseweb/index.aspx
+#'
+#' 	 \item PhosAgro Annual Report 2013: "What Quality Means To Us". Downloaded from https://phosagro.com/investors/reports/year/
+#' 	 \item PhosAgro Integrated Report 2014: "Moving Closer To Key Markets". Downloaded from https://phosagro.com/investors/reports/year/
+#' 	 \item PhosAgro Integrated Report 2015: "Creating Value For Farmers". Downloaded from https://phosagro.com/investors/reports/year/
+#' 	 \item PhosAgro Integrated Report 2016: "The Phosphate Journey: From Mine to Plate". Downloaded from https://phosagro.com/investors/reports/year/
+#' 	 \item PhosAgro Integrated Report 2017: "Growth, Efficiency, Value". Downloaded from https://phosagro.com/investors/reports/year/
+#' }
+"dapmap.markets"
---
+++ R/dapmap.markets/data-raw/market-data.R
@@ -0,0 +1,99 @@
+library(dplyr)
+
+# load regions
+d <- read.csv("data-raw/regions.csv", check.names = FALSE)
+
+regions <- Reduce(
+  function(seed, x) {
+    
+    values <- list(as.character(filter(d, Region == x)[, 2]))
+    names(values) <- c(x)
+    
+    return (append(seed, values))
+  },
+  sort(unique(as.character(d[, 1]))),
+  init = list())
+regions <- append(
+  regions, 
+  list("East Asia" = append(
+    regions[["China"]], 
+    regions[["East Asia (excl. China)"]])))
+regions["China"] <- NULL
+regions["East Asia (excl. China)"] <- NULL
+
+# load regional trade
+trade <- read.csv(
+  "data-raw/comtrade regional sales volume.csv", 
+  check.names = FALSE) %>%
+  filter(`Comtrade Reports` == "Import")
+trade['East Asia'] = Reduce(
+  function(s, x)(s + trade[, x]), 
+  c("China", "East Asia (excl. China)"), 
+  init = rep(0, nrow(trade)))
+trade <-  trade %>% 
+  select(-c(append(c("China", "East Asia (excl. China)"), "Comtrade Reports")))
+
+# load other dapmap.markets
+totals <- read.csv("data-raw/comtrade total trade.csv", check.names = FALSE)
+sales <- read.csv("data-raw/companies sales.csv", check.names = FALSE)
+in.region <- read.csv("data-raw/companies.csv", check.names = FALSE)
+consumption <- read.csv(
+  "data-raw/ifa supply db regional consumption.csv", 
+  check.names = FALSE) %>%
+  select(-c("Year"))
+
+
+# compile trade dapmap.markets
+get.home.region <- function(tag)(
+  as.character((in.region %>% filter(Company == tag))$`Home Region`[[1]]))
+
+phosagro <- "PhosAgro/EuroChem"
+phosagro.dapmap.markets <- trade %>% filter(Exporter == phosagro)
+phosagro.home <- get.home.region(phosagro)
+phosagro.dapmap.markets[, phosagro.home] <- phosagro.dapmap.markets[, phosagro.home] + 
+  (sales %>% filter(Exporter == phosagro))$Sales - 
+  (totals %>% filter(Exporter == phosagro & 
+                       `Comtrade Reports` == "Export"))[, "Sales Volume"]
+
+mosaic <- "Mosaic"
+mosaic.dapmap.markets <- trade %>% filter(Exporter == mosaic)
+mosaic.dapmap.markets[, get.home.region(mosaic)] <- 
+  (sales %>% filter(Exporter == mosaic))$Sales - 
+  (sales %>% filter(Exporter == mosaic))$Exports
+
+dapmap.markets <- trade %>% 
+  filter(Exporter %in% c("OCP", "Maaden", "China (export)")) %>%
+  rbind(phosagro.dapmap.markets, mosaic.dapmap.markets)
+  
+dapmap.markets <- dapmap.markets[, append(c("Exporter", "Year"), colnames(consumption))]
+
+years <- sort(unique(dapmap.markets$Year))
+global.supply <- Reduce(
+  function(s, y) {
+    
+    d <- as.data.frame(
+      t(
+        colSums(
+          dapmap.markets %>% 
+            filter(Year == y) %>% 
+            select(-c("Year", "Exporter")))))
+    
+    return (rbind(s, d))
+  },
+  years,
+  init = data.frame())
+
+home.dapmap.markets <- cbind(
+  data.frame(
+    Exporter = rep("Home and Other Imports", length(years)),
+    Year = years),
+  consumption - global.supply)
+
+dapmap.markets <- rbind(dapmap.markets, home.dapmap.markets)
+
+cols <- colnames(dapmap.markets)
+cols[cols == "Exporter"] <- "Producer"
+colnames(dapmap.markets) <- cols
+
+write.csv(dapmap.markets, file = "data-raw/dapmap-markets.csv", row.names = FALSE)
+usethis::use_data(dapmap.markets, overwrite = TRUE)
